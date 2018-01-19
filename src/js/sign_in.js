e073957030786bbc51917531c02eaf1524e61797
tree@global <tree-global@phone.com>
2018-01-19 12:16:16 +0800

    19.5

--- src/js/sign_in.js
+++ src/js/sign_in.js
@@ -88,9 +88,6 @@ $(function ($) {
             }, 50);
         });
 
-        //轮播图
-        $('.carousel').carousel({interval: 3000});  
-
         //纵向选项卡
         oChooseDl.on('mouseenter', function () {
             clearTimeout(iTimer);
@@ -110,62 +107,4 @@ $(function ($) {
             let iIndex = oChooseDl.index($(this));
             oListDt.eq(iIndex).css('background', '#ffffff');
         });
-
-        //倒计时
-        let intDiff = parseInt(86400); //倒计时总秒数量
-        function timer(intDiff) {
-            setInterval(function () {
-            let 
-                hour = 0,
-                minute = 0,
-                second = 0; //时间默认值
-            if (intDiff > 0) {
-                hour = Math.floor(intDiff / (60 * 60));
-                minute = Math.floor(intDiff / 60) - (hour * 60);
-                second = Math.floor(intDiff) - (hour * 60 * 60) - (minute * 60);
-            }
-            if (hour   <= 9) hour   = '0' + hour;
-            if (minute <= 9) minute = '0' + minute;
-            if (second <= 9) second = '0' + second;
-            $('#hour_show').html('<strong>' + hour + '</strong>');
-            $('#minute_show').html('<strong>' + minute + '</strong>');
-            $('#second_show').html('<strong>' + second + '</strong>');
-            intDiff--;
-            }, 1000);
-        }
-        $(function () {
-            timer(intDiff);
-        });
-
-        //横向选项卡
-        $('#uls li').hover(function () {
-            let iIndex = $(this).index();
-            $('#uls li').removeClass('hover').eq(iIndex).addClass('hover');
-            $('.act').css('display', 'none').eq(iIndex).css('display', 'block');
-        });
-
-        //手风琴
-        // $('.move .toplist').bind('mouseenter', function () {
-        //     let iIndex = $('.move').index();
-        //     $('.move').removeClass('m').eq(iIndex).addClass('m');
-        // });
-
-        //吸顶
-        // $(function(){
-        //     var TIMER;//定义全局变量
-        //     $(window).scroll( function() {
-        //         clearTimeout(TIMER);//必须要有这句
-        //         if( $(document).scrollTop() > 783 ){
-        //             TIMER = setTimeout(function(){
-        //                 $(this).addClass("nav");
-        //                 // console.log($(document).scrollTop());
-        //             },100);
-        //         }else{
-        //             TIMER = setTimeout(function(){
-        //                 $(this).removeClass("nav");
-        //             },100);
-        //         }
-        //     });
-        // });
-
     });
