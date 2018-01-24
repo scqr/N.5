08fc96a7e0b18474a46be598a8974a3a08a871f1
tree@global <tree-global@phone.com>
2018-01-24 20:48:42 +0800

    24

--- src/js/login.js
+++ src/js/login.js
@@ -1,24 +1,51 @@
 $(function ($) {
     $.noConflict();
+
+    //注册
     //用户名
     $('#txtRegUserName').focus(() => {
         $('#yes_user').addClass('img_right');
         $('#txtRegUserName').addClass('input_focus');
-        $('#tip_user').text("3～30位，由汉字、字母、数字、点、减号、下划线及\"@\"组成").removeClass('reg_errmsg');
+        $('#tip_user').text("3～20位，由汉字、字母、数字、点、减号、下划线及\"@\"组成").removeClass('reg_errmsg');
     }).blur(() => {
         var reg0 = /^[\u4e00-\u9fa5_a-zA-Z0-9\.\-\@]+$/;
         var reg1 = /^[0-9]+$/;
-        if($('#txtRegUserName').val().length < 3 || $('#txtRegUserName').val().length > 30) {
+        if($('#txtRegUserName').val().length < 3 || $('#txtRegUserName').val().length > 20) {
             $('#txtRegUserName').removeClass('input_focus').addClass('input_failure');
-            $('#tip_user').text("用户名的长度应为3～30个字符之间(汉字占两个字符)！").addClass('reg_errmsg');
+            $('#tip_user').text("用户名的长度应为3～20个字符之间(汉字占两个字符)！").addClass('reg_errmsg');
         } else if(reg1.test($('#txtRegUserName').val())) {
             $('#yes_user').addClass('img_right');
             $('#txtRegUserName').removeClass('input_focus').addClass('input_failure');
             $('#tip_user').text("用户名不能为纯数字！").addClass('reg_errmsg');
-        } else if(reg0.test($('#txtRegUserName').val())) {
-            $('#yes_user').removeClass('img_right');
-            $('#tip_user').text("");
-        }
+        } 
+        // else if(reg0.test($('#txtRegUserName').val())) {
+        //     $('#yes_user').removeClass('img_right');
+        //     $('#tip_user').text("");
+        // }
+        $.ajax({
+            type:"POST",
+            url:"php/loginCheckName.php",
+            data:{
+               "username":$("#txtRegUserName").val()
+            },
+            success:function (t) {
+                if(t == "1"){
+                    $('#yes_user').addClass('img_right');
+                    $('#txtRegUserName').removeClass('input_focus').addClass('input_failure');
+                    $('#tip_user').text("用户名已被注册！").addClass('reg_errmsg');
+                } else if($('#txtRegUserName').val().length < 3 || $('#txtRegUserName').val().length > 20) {
+                    $('#txtRegUserName').removeClass('input_focus').addClass('input_failure');
+                    $('#tip_user').text("用户名的长度应为3～20个字符之间(汉字占两个字符)！").addClass('reg_errmsg');
+                } else if(reg1.test($('#txtRegUserName').val())) {
+                    $('#yes_user').addClass('img_right');
+                    $('#txtRegUserName').removeClass('input_focus').addClass('input_failure');
+                    $('#tip_user').text("用户名不能为纯数字！").addClass('reg_errmsg');
+                } else if(t == "0" && reg0.test($('#txtRegUserName').val())) {
+                    $('#yes_user').removeClass('img_right');
+                    $('#tip_user').text("");
+                }
+            }
+        });
     });
     //密码
     var value;
@@ -98,6 +125,7 @@ $(function ($) {
     }
     var content = oVerifyCode.text();
     $('#txtVerifyCode').focus(() => {
+        console.log(1);
         $('#yes_verfycode').addClass('img_right');
         $('#txtVerifyCode').addClass('input_focus'); 
         $('#txtVerifyCodeChkMsg').text("看不清验证码时，可以点击验证码重换一张！").removeClass('reg_errmsg');
@@ -114,5 +142,85 @@ $(function ($) {
             $('#txtVerifyCode').removeClass('input_focus').addClass('input_failure');
             $('#txtVerifyCodeChkMsg').text("验证码不正确，请重新输入！").addClass('reg_errmsg');
         }
+    });
+    //协议
+    $('#elem-proto').on('click', () => {
+        $('#shadow').css("display","block");
+    });
+    //注册提交
+    var usernameExists = true;
+    $('#frmUserRegister').submit(() => {
+        $.ajax({
+            url:"php/usercheck.php",
+            method:"post",
+            data:{
+                "username":$("#txtRegUserName").val(),
+                "userpass":$('#txtRegPassword').val()
+            },
+            success:function (data) {
+                if(data=="0"){
+                    usernameExists = false;
+                    // alert("亲，您的用户名已被注册过了，请重新选一个！");
+                }else{
+                    usernameExists = true;
+                    // location.href = "../success.html";
+                }
+            }
+        });
+    });
+
+    //登录
+    //验证码
+    var oCode = $('#code');
+    oCode.text(createCode());
+    oCode.on('click', () => {
+        oCode.text(createCode());
+    });
+    function createCode() {   // 生成随机字符串
+        var sSeeds = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        var sVerify = '';
+        for(var i = 0; i < 4; i++) {
+            var iIndex = Math.floor(Math.random() * sSeeds.length);
+            sVerify += sSeeds[iIndex];
+        }
+        return sVerify;
+    }
+    var change = oCode.text();
+    $('#logincode').focus(() => {
+        console.log(1);
+        $('#yes_code').addClass('img_right');
+        $('#logincode').addClass('input_focus'); 
+        $('#tip_code').text("看不清验证码时，可以点击验证码重换一张！").removeClass('reg_errmsg');
+    }).blur(() => {
+        if($('#logincode').val() == 0) {
+            $('#yes_code').addClass('img_right');
+            $('#logincode').removeClass('input_focus').addClass('input_failure');
+            $('#tip_code').text("请输入验证码！").addClass('reg_errmsg');
+        } else if($('#logincode').val() == change) {
+            $('#yes_code').removeClass('img_right');
+            $('#tip_code').text("");
+        } else {
+            $('#yes_code').addClass('img_right');
+            $('#logincode').removeClass('input_focus').addClass('input_failure');
+            $('#tip_code').text("验证码不正确，请重新输入！").addClass('reg_errmsg');
+        }
+    });
+    $("#Image").on('click', () => {
+        $.ajax({
+           type:"POST",
+           url:"php/login.php",
+           data:{
+              "username":$("#grand").val(),
+              "userpass":$("#pa").val()
+           },
+            success:function (t) {
+                if(t == "1"){
+                    //保存cookie
+                    location.href = "../index.html";
+                }else{
+                    $("#txtPassword").html("用户名或者密码错误，请您重新输入！");
+                }
+            }
+        });
     });
 });
